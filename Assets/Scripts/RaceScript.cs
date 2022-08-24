4613288b11aa44ce75e4356f9d608b35bfccc50b
BerkcanYildiz <brkcnyldz66@gmail.com>
2022-08-24 15:24:47 +0300

    Added Timer
    Added lap timer to the top left corner and timer script to the script folder.

---
+++ Assets/Scripts/RaceScript.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaceScript : MonoBehaviour
+{
+    public float laptime;
+    private bool startTimer = false;
+    public UnityEngine.UI.Text Ltime;
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (startTimer == true)
+        {
+            laptime = laptime +  Time.deltaTime;    
+            // Debug.Log(laptime);
+            Ltime.text = laptime.ToString("F3");
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("test");
+
+        startTimer = true;
+    }
+}
