4714633dde093176c152e32b93145011545f8dfc
BerkcanYildiz <brkcnyldz66@gmail.com>
2022-08-29 19:59:57 +0300

    Added After Collision Menu
    Added after collision menu.If you hit the wall its opening and you can choose quit or restart level

--- Assets/Scripts/RestartMenu.cs
+++ Assets/Scripts/RestartMenu.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RestartMenu : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+{   
+    
+    public string afterCollisionLevel;
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void restartGame()
+    {
+        
+        SceneManager.LoadScene(afterCollisionLevel);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
