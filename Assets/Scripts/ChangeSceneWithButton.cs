3d1055b208700b9d0e5670dc837ff812e2a92b7f
BerkcanYildiz <brkcnyldz66@gmail.com>
2022-09-03 15:02:50 +0300

    CollisionMenu re-added to project

--- Assets/Scripts/ChangeSceneWithButton.cs
+++ Assets/Scripts/ChangeSceneWithButton.cs
@@ -1,10 +1,17 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ChangeSceneWithButton : MonoBehaviour
 {
+   public string afterCollisionMenu;  
    public void LoadScene(string sceneName)
    {
       SceneManager.LoadScene(sceneName);
+   }
+
+   private void OnCollisionEnter(Collision collision)
+   {
+      SceneManager.LoadScene(afterCollisionMenu);
    }
 }
