15c298b30aff44c103d4e08abca7b1ee4e317f42
BerkcanYildiz <brkcnyldz66@gmail.com>
2022-09-12 19:00:07 +0300

    Update ChangeSceneWithButton.cs

--- Assets/Scripts/ChangeSceneWithButton.cs
+++ Assets/Scripts/ChangeSceneWithButton.cs
@@ -3,14 +3,8 @@ using UnityEngine.SceneManagement;
 
 public class ChangeSceneWithButton : MonoBehaviour
 {
-   // [SerializeField] private string afterCollisionMenu;  
    public void LoadScene(string sceneName)
    {
       SceneManager.LoadScene(sceneName);
    }
-
-   /* private void OnCollisionEnter(Collision collision)
-   {
-      SceneManager.LoadScene(afterCollisionMenu);
-   } */
 }
